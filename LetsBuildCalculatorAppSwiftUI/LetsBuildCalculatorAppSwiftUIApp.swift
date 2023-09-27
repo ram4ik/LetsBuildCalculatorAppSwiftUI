8d0d59beb52f4e6b4be1648eb914cae88e7420ca
Ramill <ram4ik@hotmail.com>
2023-09-27 14:04:52 +0300

    Initial Commit

---
+++ LetsBuildCalculatorAppSwiftUI/LetsBuildCalculatorAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LetsBuildCalculatorAppSwiftUIApp.swift
+//  LetsBuildCalculatorAppSwiftUI
+//
+//  Created by Ramill Ibragimov on 9/27/23.
+//
+
+import SwiftUI
+
+@main
+struct LetsBuildCalculatorAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
